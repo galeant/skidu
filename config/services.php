993f562ba6ffbb4b9cdf9b457beec2bae8fbbaac
ilham <pigijopigijo@gmail.com>
2018-11-01 02:53:46 +0700

    env

--- config/services.php
+++ config/services.php
@@ -36,10 +36,13 @@ return [
     ],
 
     'facebook' => [
-        'client_id'     => '183886865850577',
-        'client_secret' => '0dfe422d140fba9699757bbdc055f0e8',
-        'redirect'      => 'http://skidu.herokuapp.com/login/facebook/callback',
+        'client_id'     => ENV('FACEBOOK_CLIENT_ID'),
+        // 'client_id'     => '183886865850577',
+        'client_secret' => ENV('FACEBOOK_CLIENT_SECRET'),
+        // 'client_secret'     =>'0dfe422d140fba9699757bbdc055f0e8',
+        'redirect'      => ENV('APP_URL').'login/facebook/callback',
         // 'redirect'      => 'http://localhost:8000/login/facebook/callback',
+        // 'redirect'      => 'http://skidu.herokuapp.com/login/facebook/callback',
     ],
 
 ];
