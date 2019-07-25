85ae46023a38dd38034b0eccc510ac111985dfdd
ilham <ilham.fadillah@gmail.com>
2019-07-25 14:16:33 +0700

    revert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,21 +85,17 @@
             @endif
 
             <div class="content">
-                <!-- <div class="title m-b-md">
+                <div class="title m-b-md">
                     Laravel
-                </div> -->
+                </div>
 
                 <div class="links">
-                    <a class="btn bg-blue" href="{{ url('contoh_1') }}">Template 1</a>
-                    <a class="btn bg-blue" href="{{ url('contoh_2') }}">Template 2</a>
-                    <a class="btn bg-blue" href="{{ url('contoh_3') }}">Template 3</a>
-                    <a class="btn bg-blue" href="{{ url('contoh_4') }}">Template 4</a>
-                    <!-- <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://nova.laravel.com">Nova</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
